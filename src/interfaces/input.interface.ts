cbea0828a66d04993db080131e41c2a9c2fb56e5
Winnie Brandão <winnie_brandao@yahoo.com.br>
2023-04-12 15:18:30 +0100

    feat: add input component

---
+++ src/interfaces/input.interface.ts
@@ -0,0 +1,9 @@
+import { FieldError, UseFormRegisterReturn } from "react-hook-form";
+
+export interface IInput {
+  label: string;
+  type: "text" | "email" | "password";
+  register: UseFormRegisterReturn<string>;
+  error?: FieldError;
+  defaultValue: string | undefined;
+}
