b167ff533abc9bbc86707d195dc85f9f8bc42f67
Winnie Brandão <winnie_brandao@yahoo.com.br>
2023-04-13 13:30:46 +0100

    feat: add full merge

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,7 @@
-<<<<<<< HEAD
 import { Modal } from "./components/Modal";
 
 function App() {
   return <Modal />;
-=======
-import { Routes } from "react-router-dom";
-import "./App.css";
-import AppRoutes from "./routes";
-import { Global } from "./styles/global";
-
-function App() {
-  
-  return (
-    <>
-      <Global/>
-      <AppRoutes/>
-    </>
-  )  
-  
->>>>>>> 93711afcc066a1c23f0d7c5e305d48f710397e92
 }
 
 export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,30 +1,14 @@
-<<<<<<< HEAD
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { Global } from "./styles/global";
 import { BrowserRouter } from "react-router-dom";
-=======
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
-import App from './App'
-import './index.css'
->>>>>>> 93711afcc066a1c23f0d7c5e305d48f710397e92
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <BrowserRouter>
-<<<<<<< HEAD
       <Global />
       <App />
     </BrowserRouter>
   </React.StrictMode>
 );
-=======
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>
-
-)
->>>>>>> 93711afcc066a1c23f0d7c5e305d48f710397e92
