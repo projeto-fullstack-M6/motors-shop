68cf8d68856f864215ef51ea28bc66380596c6ac
Lucas Rodrigues <lucasvplayer@gmail.com>
2023-05-05 10:01:35 -0300

    Fix: Api errors

--- src/services/index.tsx
+++ src/services/index.tsx
@@ -3,7 +3,7 @@ import axios from "axios";
 const token = localStorage.getItem("@motors:token");
 
 export const ApiRequests = axios.create({
-  baseURL: "https://motors-api-deploy.onrender.com/",
+  baseURL: "https://motors-api-deploy.onrender.com",
   timeout: 150000,
   headers: {
     "Content-Type": "application/json",
